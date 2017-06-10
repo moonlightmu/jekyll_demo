1e45dba9033f2cd91ec4371ec83e0c43c6ab66fa
moonlightmu <moonlightmu@363850855@qq.com>
2017-06-10 23:40:37 +0800

    df

--- js/action.js
+++ js/action.js
@@ -1,11 +1,13 @@
 function init(){
-var phraseElement=document.getElementById("phrase");
+var phraseElement1=document.getElementById("phrase1");
+var phraseElement2=document.getElementById("phrase2");
 var wordSet1=["Java","Css","Javascript","C","Html","Php"];
-var wordSet2=["love","happy","healthy","money","courage"];
+var wordSet2=["Love","Happy","Healthy","Money","Courage"];
 var rand1=Math.floor(Math.random() * wordSet1.length);
 var rand2=Math.floor(Math.random() * wordSet2.length);
-var phrase=wordSet1[rand1]+""+wordSet2[rand2];
-phraseElement.innerHTML=phrase;
-alert("hell");
+var phrase1=wordSet1[rand1];
+var phrase2=wordSet2[rand2];
+phraseElement1.innerHTML=phrase1;
+phraseElement2.innerHTML=phrase2;
 }
 window.onload=init;
