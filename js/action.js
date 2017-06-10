b6fd709f23b035bb83e45209648e335ced739e35
moonlightmu <moonlightmu@363850855@qq.com>
2017-06-10 22:25:16 +0800

    df

--- js/action.js
+++ js/action.js
@@ -1,5 +1,11 @@
 function init(){
-var myInfo=document.getElementById("myphoto");
-myInfo.style.color=#cc6600;
+var phraseElement=document.getElementById("phrase");
+var wordSet1=["Java","Css","Javascript","C","Html","Php"];
+var wordSet2=["love","happy","healthy","money","courage"];
+var rand1=Math.floor(Math.random() * wordSet1.length);
+var rand2=Math.floor(Math.random() * wordSet2.length);
+var phrase=wordSet1[rand1]+""+wordSet2[rand2];
+var phraseElement.innerHTML=phrase;
+
 }
 window.onload=init;
