1b4af0ee67ce24b7409bcb7477f8c23486e814cc
moonlightmu <moonlightmu@363850855@qq.com>
2017-06-11 20:59:51 +0800

    df

--- js/action.js
+++ js/action.js
@@ -8,11 +8,11 @@ var img1=document.createElement("img");
 
 var rand1=Math.floor(Math.random() * photos.length);
 img1.setAttribute("src","../img/"+photos[rand1]);
-
+alert(img1.style.src);
 
 var section1=document.getElementById("main");
 section1.appendChild(div1);
-alert(img1.src);
+
 }
 }
 
