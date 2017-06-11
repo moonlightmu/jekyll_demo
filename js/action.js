e97f8c3a13decb110e57d421362a515fb2787702
moonlightmu <moonlightmu@363850855@qq.com>
2017-06-11 21:23:27 +0800

    df

--- js/action.js
+++ js/action.js
@@ -15,7 +15,7 @@ div1.setAttribute("id","container");
 p1.innerHTML=photos[rand1];
 
 div1.appendChild(p1);
-img1.appendChild(div1);
+img1.insertBefore(div1,img1.nextSibling);
 
 var section1=document.getElementById("main");
 section1.appendChild(img1);
