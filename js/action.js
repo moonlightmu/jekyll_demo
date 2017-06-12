5f3881bedc1b993e4bd0112fd2beb58ae1ecc3e6
moonlightmu <moonlightmu@363850855@qq.com>
2017-06-13 00:53:24 +0800

    df

--- js/action.js
+++ js/action.js
@@ -46,7 +46,7 @@ mapTypeId: google.maps.MapTypeId.ROADMAP
 
 var mapDiv=document.getElementById("map");
 map=new google.maps.Map(mapDiv,mapOptions);
-alert(map.id);
+alert(map);
 }
 
 function displayLocation(position){
