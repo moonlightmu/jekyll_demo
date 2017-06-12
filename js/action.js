0661ef2c78c6b333c929b7fb98047c6c52ab2092
moonlightmu <moonlightmu@363850855@qq.com>
2017-06-12 22:23:18 +0800

    df

--- js/action.js
+++ js/action.js
@@ -1,23 +1,4 @@
-function displayError(error){
-var errorTypes={
-0: "Unknown error",
-1: "Permission denied by user",
-2: "Position is not available",
-3: "Request timed out"
-};
-var errorMessage=errorTypes[error.code];
-if(error.code==0 || error.code==2){
-errorMessage=errorMessage+""+error.message;
-}
-var div=document.getElementById("location");
-div.innerHTML=errorMessage;
-}
-function displayLocation(position){
-var latitude=position.coords.latitude;
-var longitude=position.coords.longitude;
-var div=document.getElementById("location");
-div.innerHTML="you are at latitude:"+latitude+",longitude:"+longitude;
-}
+
 function init2(){
 var i;
 var j;
