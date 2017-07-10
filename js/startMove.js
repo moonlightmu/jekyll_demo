8590c52c6abbcb9d553f5d42ed7a0243901c08cc
moonlightmu <moonlightmu@363850855@qq.com>
2017-07-10 22:39:36 +0800

    df

--- js/startMove.js
+++ js/startMove.js
@@ -1,20 +1,42 @@
-function startMoveinfo(obj,iTarget){	
-	var speed;
+function getStyle(obj,name){
+	if(obj.currentStyle){
+		return obj.currentStyle[name];
+	}
+	else{
+		return obj.getComputedStyle(obj,false)[name];
+	}	
+}
+function startMove(obj,attr,iTarget){	
 	clearInterval(obj.timer);
 	obj.timer=setInterval(function(){
-		speed=iTarget>0?3:-3;
-		if(obj.offsetLeft>=700||speed==0){
+		var cur;
+		if(attr=='opacity'){
+		 cur=parseFloat(getStyle(obj,attr))*100;
+		}else
+		 cur=parseInt(getStyle(obj,attr));
+
+		speed=(iTarget-cur)/10;
+	    if(speed>0)
+		{speed=Math.ceil(speed);}
+		else
+		{
+			speed=Math.floor(speed);
+		}
+	    
+		if(cur==iTarget||speed==0){
 			clearInterval(obj.timer);
 		}
 		else{
-			speed=(iTarget-obj.offsetLeft)/7;
-			    if(speed>0)
-				{speed=Math.ceil(speed);}
-				else
-				{speed=Math.floor(speed);
-				}		
-			obj.style.left=obj.offsetLeft+speed+'px';
-			document.title=obj.offsetLeft+","+speed;
+					
+			if(attr=='opacity'){
+				
+			   //ie
+				obj.style.filter='alpha(opacity:'+(cur+speed)+')';
+				//ff,chrome
+					obj.style.opacity=(cur+speed)/100;
+			}
+			obj.style[attr]=cur+speed+'px';
+			document.title=cur+","+speed;
 		}	
 	},30);	
 }
